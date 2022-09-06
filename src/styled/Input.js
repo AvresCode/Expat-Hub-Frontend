d061a486dc2af524056bd9fbbad38747f974872d
Serva <servas230@gmail.com>
2022-09-06 15:44:39 +0200

    Frontend setup done!

---
+++ src/styled/Input.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+  width: 50%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  ::placeholder {
+    color: #1e3163;
+  }
+`;
