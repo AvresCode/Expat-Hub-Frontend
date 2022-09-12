9b19587cf88b7e39016b40c6eda829cc5b9cf06c
Serva <servas230@gmail.com>
2022-09-12 20:03:55 +0200

    Modified signup form

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -7,9 +7,16 @@ import { signUp } from "../store/user/thunks";
 import { selectToken } from "../store/user/selectors";
 
 export const SignUp = () => {
-  const [name, setName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [city, setCity] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [gender, setGender] = useState("");
+  const [nationality, setNationality] = useState("");
+  const [education, setEducation] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -24,7 +31,21 @@ export const SignUp = () => {
 
   const submitForm = (e) => {
     e.preventDefault();
-    dispatch(signUp(name, email, password));
+    console.log("submit works");
+    dispatch(
+      signUp(
+        firstName,
+        lastName,
+        email,
+        password,
+        city,
+        birthDate,
+        gender,
+        nationality,
+        education,
+        imageUrl
+      )
+    );
   };
 
   return (
@@ -32,22 +53,94 @@ export const SignUp = () => {
       <Container>
         <Title>Sign Up</Title>
         <form onSubmit={submitForm}>
-          <Input
-            placeholder="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <Input
-            placeholder="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            type="password"
-            placeholder="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <div>
+            <label>First Name:</label>
+          </div>
+          <div>
+            {" "}
+            <Input
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Last Name:</label>
+          </div>
+          <div>
+            {" "}
+            <Input
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>Email:</label>
+          </div>
+          <div>
+            {" "}
+            <Input value={email} onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div>
+            <label>Password:</label>
+          </div>
+          <div>
+            {" "}
+            <Input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <label>City:</label>
+          </div>
+          <div>
+            {" "}
+            <Input value={city} onChange={(e) => setCity(e.target.value)} />
+          </div>
+          <div>
+            <label>Date of birth:</label>
+          </div>
+          <div>
+            <div>
+              {" "}
+              <Input
+                type="date"
+                value={birthDate}
+                onChange={(e) => setBirthDate(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Gender:</label>
+            </div>
+            <div>
+              {" "}
+              <Input
+                value={gender}
+                onChange={(e) => setGender(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Nationality:</label>
+            </div>
+            <div>
+              {" "}
+              <Input
+                value={city}
+                onChange={(e) => setNationality(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>Education:</label>
+            </div>
+            <div>
+              {" "}
+              <Input
+                value={education}
+                onChange={(e) => setEducation(e.target.value)}
+              />
+            </div>
+          </div>
           <br />
           <Button type="submit">Sign Up</Button>
         </form>
