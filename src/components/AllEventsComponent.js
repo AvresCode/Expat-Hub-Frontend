f2a03599a3e88e57837301b4941d5694c5f06ed1
AvresCode <servas230@gmail.com>
2023-04-05 18:20:08 +0200

    refactor all events page

--- src/components/AllEventsComponent.js
+++ src/components/AllEventsComponent.js
@@ -1,38 +1,36 @@
-import { fetchAllEvents } from "../store/event/thunks";
-import { useEffect, useState } from "react";
-import { selectAllEvents } from "../store/event/selectors";
-import { useDispatch, useSelector } from "react-redux";
-import { EventCard } from "./EventCard";
-import { AllEventsContainer, Input, Button } from "../styled";
-import moment from "moment";
+import { fetchAllEvents } from '../store/event/thunks';
+import { useEffect, useState } from 'react';
+import { selectAllEvents } from '../store/event/selectors';
+import { useDispatch, useSelector } from 'react-redux';
+import { EventCard } from './EventCard';
+import { AllEventsContainer, Input } from '../styled';
+import moment from 'moment';
 
 export const AllEventsComponent = () => {
-  const [search, setSearch] = useState("");
-  const [searchDate, setSearchDate] = useState("");
+  const [search, setSearch] = useState('');
+  const [searchDate, setSearchDate] = useState('');
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchAllEvents);
   }, [dispatch]);
 
   const allEvents = useSelector(selectAllEvents);
-  // console.log("allEvents", allEvents);
 
-  if (!allEvents)
-    return (
-      <div>
-        {" "}
-        <p> Loading ...</p>
-      </div>
+  if (!allEvents) return <div>Loading ...</div>;
+
+  const filteredPastEvents = [...allEvents].filter((event) =>
+    moment(event.date).isAfter()
+  );
+
+  const sortedEventDate = filteredPastEvents.sort((a, b) =>
+    moment(a.date).diff(b.date)
+  );
+
+  const eventData = sortedEventDate
+    .filter((event) => event.title.toLowerCase().includes(search.toLowerCase()))
+    .filter((event) =>
+      searchDate ? moment(searchDate).isSame(moment(event.date), 'day') : true
     );
-
-  const filteredPastEvents = [...allEvents].filter(
-    (event) => new Date(event.date) > new Date()
-  );
-  // console.log("filtered event", filteredPastEvents);
-
-  const sortedEventDate = filteredPastEvents.sort(
-    (a, b) => new Date(a.date) - new Date(b.date)
-  );
 
   const eventToAttendeesList = (event) => {
     return event.going.map((user) => ({
@@ -42,69 +40,47 @@ export const AllEventsComponent = () => {
   };
 
   return (
-    <div style={{ width: "85vw" }}>
+    <div style={{ width: '85vw' }}>
       <Input
         type="text"
         placeholder="Search for event.."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        style={{ width: "70vw" }}
+        style={{ width: '70vw' }}
       />
       <div>
-        {" "}
-        <h3>Pick a date: </h3>
-        <div>
-          {" "}
-          <form>
-            <Input
-              style={{ width: "15vw" }}
-              type="date"
-              value={searchDate}
-              onChange={(e) => setSearchDate(e.target.value)}
-            />
-          </form>
-        </div>
+        {' '}
+        <h2>Pick a date: </h2>
+        <Input
+          style={{ width: '15vw' }}
+          type="date"
+          value={searchDate}
+          onChange={(e) => setSearchDate(e.target.value)}
+        />
       </div>
       <AllEventsContainer>
-        {" "}
-        {sortedEventDate
-          .filter((event) =>
-            event.title.toLowerCase().includes(search.toLowerCase())
-          )
-          .filter((event) =>
-            searchDate
-              ? moment(searchDate).isSame(moment(event.date), "day")
-              : true
-          )
-          .map((event) => {
-            const {
-              id,
-              imageUrl,
-              title,
-              city,
-              date,
-              description,
-              spots,
-              going,
-            } = event;
-            return (
-              <div key={id}>
-                <EventCard
-                  id={id}
-                  imageUrl={imageUrl}
-                  title={title}
-                  city={city}
-                  date={date}
-                  description={description}
-                  spots={spots}
-                  going={going}
-                  showDetails={false}
-                  showLink={true}
-                  attendees={eventToAttendeesList(event)}
-                />
-              </div>
-            );
-          })}
+        {' '}
+        {eventData.map((event) => {
+          const { id, imageUrl, title, city, date, description, spots, going } =
+            event;
+          return (
+            <div key={id}>
+              <EventCard
+                id={id}
+                imageUrl={imageUrl}
+                title={title}
+                city={city}
+                date={date}
+                description={description}
+                spots={spots}
+                going={going}
+                showDetails={false}
+                showLink={true}
+                attendees={eventToAttendeesList(event)}
+              />
+            </div>
+          );
+        })}
       </AllEventsContainer>
     </div>
   );
