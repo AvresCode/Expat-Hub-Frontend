9ef3309362f6848066df25994277c374d61b39b7
AvresCode <servas230@gmail.com>
2023-09-08 23:24:24 +0200

    solve bug with mozilla autofill input color

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,22 +1,22 @@
-import styled from "styled-components";
-import { Button, Input, Title } from "../styled";
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { signUp } from "../store/auth/thunks";
-import { selectToken } from "../store/auth/selectors";
+import styled from 'styled-components';
+import { Button, Input, Title } from '../styled';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { signUp } from '../store/auth/thunks';
+import { selectToken } from '../store/auth/selectors';
 
 export const SignUp = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [city, setCity] = useState("");
-  const [birthDate, setBirthDate] = useState("");
-  const [gender, setGender] = useState("");
-  const [nationality, setNationality] = useState("");
-  const [education, setEducation] = useState("");
-  const [imageUrl, setImageUrl] = useState("");
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [city, setCity] = useState('');
+  const [birthDate, setBirthDate] = useState('');
+  const [gender, setGender] = useState('');
+  const [nationality, setNationality] = useState('');
+  const [education, setEducation] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -25,34 +25,34 @@ export const SignUp = () => {
 
   useEffect(() => {
     if (token !== null) {
-      navigate("/");
+      navigate('/');
     }
   }, [token, navigate]);
 
   const uploadImage = async (e) => {
     const files = e.target.files;
     const data = new FormData();
-    data.append("file", files[0]);
+    data.append('file', files[0]);
 
-    data.append("upload_preset", "lbazsi6x");
+    data.append('upload_preset', 'lbazsi6x');
 
     const res = await fetch(
-      "https://api.cloudinary.com/v1_1/df03t7txo/image/upload",
+      'https://api.cloudinary.com/v1_1/df03t7txo/image/upload',
       {
-        method: "POST",
+        method: 'POST',
         body: data,
       }
     );
 
     const file = await res.json();
-    console.log("file", file);
+    console.log('file', file);
     setImageUrl(file.url);
   };
 
   const submitForm = (e) => {
     e.preventDefault();
     console.log(
-      "submit works",
+      'submit works',
       firstName,
       lastName,
       email,
@@ -78,20 +78,20 @@ export const SignUp = () => {
         imageUrl
       )
     );
-    setFirstName("");
-    setLastName("");
-    setEmail("");
-    setPassword("");
-    setCity("");
-    setBirthDate("");
-    setGender("");
-    setNationality("");
-    setEducation("");
-    setImageUrl("");
+    setFirstName('');
+    setLastName('');
+    setEmail('');
+    setPassword('');
+    setCity('');
+    setBirthDate('');
+    setGender('');
+    setNationality('');
+    setEducation('');
+    setImageUrl('');
   };
 
   return (
-    <div style={{ textAlign: "center" }}>
+    <div style={{ textAlign: 'center' }}>
       <Container>
         <Title>Sign Up</Title>
         <form onSubmit={submitForm}>
@@ -99,7 +99,7 @@ export const SignUp = () => {
             <label>First Name:</label>
           </div>
           <div>
-            {" "}
+            {' '}
             <Input
               value={firstName}
               onChange={(e) => setFirstName(e.target.value)}
@@ -110,7 +110,7 @@ export const SignUp = () => {
             <label>Last Name:</label>
           </div>
           <div>
-            {" "}
+            {' '}
             <Input
               value={lastName}
               onChange={(e) => setLastName(e.target.value)}
@@ -121,30 +121,32 @@ export const SignUp = () => {
             <label>Email:</label>
           </div>
           <div>
-            {" "}
+            {' '}
             <Input
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               required
+              className="autofill-bg-fix"
             />
           </div>
           <div>
             <label>Password:</label>
           </div>
           <div>
-            {" "}
+            {' '}
             <Input
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               required
+              className="autofill-bg-fix"
             />
           </div>
           <div>
             <label>City:</label>
           </div>
           <div>
-            {" "}
+            {' '}
             <Input
               value={city}
               onChange={(e) => setCity(e.target.value)}
@@ -156,7 +158,7 @@ export const SignUp = () => {
           </div>
           <div>
             <div>
-              {" "}
+              {' '}
               <Input
                 type="date"
                 value={birthDate}
@@ -168,7 +170,7 @@ export const SignUp = () => {
               <label>Gender:</label>
             </div>
             <div>
-              {" "}
+              {' '}
               <Input
                 value={gender}
                 onChange={(e) => setGender(e.target.value)}
@@ -178,7 +180,7 @@ export const SignUp = () => {
               <label>Nationality:</label>
             </div>
             <div>
-              {" "}
+              {' '}
               <Input
                 value={nationality}
                 onChange={(e) => setNationality(e.target.value)}
@@ -188,7 +190,7 @@ export const SignUp = () => {
               <label>Education:</label>
             </div>
             <div>
-              {" "}
+              {' '}
               <Input
                 value={education}
                 onChange={(e) => setEducation(e.target.value)}
@@ -198,15 +200,15 @@ export const SignUp = () => {
               <label>Upload photo:</label>
             </div>
             <div>
-              {" "}
+              {' '}
               <Input type="file" onChange={uploadImage} />
             </div>
             <div>
-              <img alt="" width={200} src={imageUrl ? imageUrl : ""} />
+              <img alt="" width={200} src={imageUrl ? imageUrl : ''} />
               {imageUrl ? (
                 <Title style={{ fontSize: 15 }}>Succesfully uploaded!</Title>
               ) : (
-                ""
+                ''
               )}
             </div>
           </div>
@@ -219,7 +221,7 @@ export const SignUp = () => {
 };
 
 const Container = styled.div`
-  display: "flex";
-  flex-direction: "column";
+  display: 'flex';
+  flex-direction: 'column';
   margin: 15%;
 `;
